9c00a1937d603676e532505f2ee706a844394a0a
Khloe Leclair <xivkhloeleclair@gmail.com>
2022-04-30 16:41:26 -0400

    WIP ThemeManager as a Framework Mod

---
+++ ThemeManager/ModConfig.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leclair.Stardew.ThemeManager;
+
+internal class ModConfig {
+
+	public Dictionary<string, string> SelectedThemes { get; set; } = new();
+
+}
